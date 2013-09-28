32abe8449002e80e795827262b012a7013ee0bf3
John-Philip Johansson <seriema@gmail.com>
2013-09-28 21:58:15 +0200

    First commit.

---
+++ LinAlg/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+#light
+
+open LinAlg.Math
+    
+
+let v1 = Vector(1.0, 2.0)
+
+let v2 = v1 * 2.0
+let v3 = 2.0 * v1
+
+let v4 = - v2
+
+let s1 = LinAlg.Math.dot v1 v2
+
+let v5 = LinAlg.Math.cross (Vector3(1.0,2.0,3.0)) (Vector3(-1.0,2.0,3.0))
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%s" (v1.ToString())
+    printfn "%s" (v2.ToString())
+    printfn "%s" (v3.ToString())
+    printfn "%s" (v4.ToString())
+    printfn "%s" (s1.ToString())
+    printfn "%s" (v5.ToString())
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ Math/Vector.fs
@@ -0,0 +1,29 @@
+﻿module LinAlg.Math
+    type Vector(x: float, y: float) =
+        member this.x = x
+        member this.y = y
+        static member (~-) (v : Vector) =
+            Vector(-1.0 * v.x, -1.0 * v.y)
+        static member (*) (v : Vector, a) =
+            Vector(a * v.x, a * v.y)
+        static member (*) (a, v: Vector) =
+            Vector(a * v.x, a * v.y)
+        override this.ToString() =
+            this.x.ToString() + " " + this.y.ToString()
+
+    type Vector3(x: float, y: float, z: float) =
+        member this.x = x
+        member this.y = y
+        member this.z = z
+        override this.ToString() =
+            this.x.ToString() + " " + this.y.ToString() + " " + this.z.ToString()
+
+
+    let dot (v:Vector) (u:Vector) =
+        v.x * u.x + v.y * u.y
+
+    let cross (v:Vector3) (u:Vector3) =
+        let x = v.y*u.z + v.z*u.y
+        let y = v.x*u.z + v.z*u.x
+        let z = v.x*u.y + v.y*u.x
+        Vector3(x, y, z)
