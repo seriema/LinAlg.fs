a77be798b68b3d6d549898d5bfa8d33eb6a74f38
John-Philip Johansson <seriema@gmail.com>
2013-09-29 19:41:09 +0200

    Move Vector3 to own file

--- Math/Vector.fs
+++ Math/Vector.fs
@@ -15,13 +15,6 @@
         override this.ToString() =
             this.x.ToString() + " " + this.y.ToString()
 
-    type Vector3(x: float, y: float, z: float) =
-        member this.x = x
-        member this.y = y
-        member this.z = z
-        override this.ToString() =
-            this.x.ToString() + " " + this.y.ToString() + " " + this.z.ToString()
-
 
     let dot (v:Vector) (u:Vector) =
         v.x * u.x + v.y * u.y
@@ -38,11 +31,3 @@
     let prep (v:Vector) (u:Vector) =
         u - proj v u
 
-//    let dot (v:Vector3) (u:Vector3) =
-//        v.x * u.x + v.y * u.y + v.z * u.z
-
-    let cross (v:Vector3) (u:Vector3) =
-        let x = v.y*u.z + v.z*u.y
-        let y = v.x*u.z + v.z*u.x
-        let z = v.x*u.y + v.y*u.x
-        Vector3(x, y, z)
---
+++ Math/Vector3.fs
@@ -0,0 +1,17 @@
+﻿module Linalg.Math
+
+    type Vector3(x: float, y: float, z: float) =
+        member this.x = x
+        member this.y = y
+        member this.z = z
+        override this.ToString() =
+            this.x.ToString() + " " + this.y.ToString() + " " + this.z.ToString()
+
+    let dot (v:Vector3) (u:Vector3) =
+        v.x * u.x + v.y * u.y + v.z * u.z
+
+    let cross (v:Vector3) (u:Vector3) =
+        let x = v.y*u.z + v.z*u.y
+        let y = v.x*u.z + v.z*u.x
+        let z = v.x*u.y + v.y*u.x
+        Vector3(x, y, z)
