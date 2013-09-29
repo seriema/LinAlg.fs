7678fe9490cbca5df706cd73e7a8e84f6212a2e4
John-Philip Johansson <seriema@gmail.com>
2013-09-29 20:58:59 +0200

    Add unit tests with NUnit

---
+++ LinAlg.Tests/Vector2.dot.fs
@@ -0,0 +1,14 @@
+﻿module LinAlg.Tests
+
+open NUnit.Framework
+open LinAlg.Math
+
+[<TestFixture>]
+type Vector2dot() =
+
+    [<Test>]
+    member self.perpendicular_vectors_have_zero_dot_product() =
+        let v = Vector2(1.,0.)
+        let u = Vector2(0.,1.)
+        let s = dot v u
+        Assert.AreEqual(0., s)
